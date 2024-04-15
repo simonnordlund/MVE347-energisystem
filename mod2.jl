6482004d0cb939289a339e2ba3ac685b19686bc1
Simon Nordlund <simonnor@chalmers.se>
2024-04-15 14:42:50 +0200

    Tried to copy over the changes from mod 1 to mod 2

--- mod2.jl
+++ mod2.jl
@@ -2,22 +2,35 @@ function build_energy_model(data_file::String)
     include(data_file)  
     #name the model
     m = Model()
-
-    
     r=0.05
     @variable(m, x[I,J,S] >= 0) #Variable hour, amount of MWH for technology i for country j for a certian hour s.
-    @variable(m,z[I,J] >= 0) #The capacity, amount of kw for technology i for country j
+    @variable(m,z[I,J] >= 0) #The capacity, amount of MW for technology i for country j
     @variable(m,volume[S] >= 0)
+    @variable(m, Annualized_Investment[i in I, j in J] >= 0)
+    @variable(m,Fuel_Cost[i in I, j in J] >= 0)
+    @variable(m, Running_Cost[i in I, j in J] >= 0)
+    @variable(m, Fuel[i in I, j in J, s in S] >= 0 )
+    @variable(m, Emission[i in I, j in J] >= 0 )
     @variable(m,batterystorage[J,S] >= 0) # amount of MWH for a battery in country j during an hour s.
+
+
+
+
     #minimize the cost
    
-    @objective(m, Min, sum(run_cost[i] * sum(x[i,j,s] / efficiency[i] for s in S) for j in J for i in I)
-    + sum(inv_cost[i] * r/(1-1/(1+r)^lifetime[i]) * sum(z[i,j] for j in J) for i in I)
-    +fuel_cost[3]/efficiency[3]*sum(x[3,j,s] for j in J for s in S ))
+    @objective(m, Min, sum(Running_Cost) + sum(Annualized_Investment) + sum(Fuel_Cost) )
   
-    @constraint(m,[i in [1,2,4]], z[i,1] <= cap_de[i]) #maximun capacity for Germany
-    @constraint(m,[i in [1,2,4]], z[i,2] <= cap_swe[i]) #maximun capacity for Sweden
-    @constraint(m,[i in [1,2,4]], z[i,3] <= cap_dk[i]) #maximun capacity for Denmark
+
+    @constraint(m, ANNUALIZED_INVESTMENT[i in I, j in J], Annualized_Investment[i,j] >= inv_cost[i] * r/(1-1/(1+r)^lifetime[i]) * z[i,j])
+    @constraint(m, FUEL_COST[i in I, j in J], Fuel_Cost[i,j] >= fuel_cost[i]/efficiency[i]*sum(x[i,j,s] for s in S ) )
+    @constraint(m, RUNNING_COST[i in I, j in J], Running_Cost[i,j] >= run_cost[i] * sum(x[i,j,s] / efficiency[i] for s in S) )
+
+    @constraint(m, EFFICIENCY[i in I, j in J, s in S], Fuel[i,j,s] == x[i,j,s]/efficiency[i] )#Amount of fuel used.
+    @constraint(m, EMISSION[i in I, j in J], Emission[i,j] == emiss_factor[i] * sum(Fuel[i,j,s] for s in S))
+
+    @constraint(m,[i in [1,2,4]], z[i,1] <= cap_de[i]) #maximum capacity for Germany
+    @constraint(m,[i in [1,2,4]], z[i,2] <= cap_swe[i]) #maximum capacity for Sweden
+    @constraint(m,[i in [1,2,4]], z[i,3] <= cap_dk[i]) #maximum capacity for Denmark
 
     @constraint(m,[s in S],z[1,1]*Wind_DE[s] >= x[1,1,s]) #Maximum possible production wind DE
     @constraint(m,[s in S],z[1,2]*Wind_SE[s] >= x[1,2,s]) #Maximum possible production wind SE
@@ -34,20 +47,19 @@ function build_energy_model(data_file::String)
 
     #@constraint(m,volume[1]==14*10^3) #first hour of water
     #Reservoir >=0 and >=max for every hour
+
     for hour in 2:length(time_arr)
         @constraint(m,volume[hour] == volume[hour-1] - x[4,2,hour-1] + Hydro_inflow[hour-1]) #Waterflow each hour
     end
-
+    @constraint(m,[i in [1,3],s in S], x[4,i,s] == 0) #No water in DK & DE
     @constraint(m,volume[1] == volume[end] - sum(x[4,j,end] for j in J) + Hydro_inflow[end]) #Circular flow
     
-    @constraint(m,[i in [1,3],s in S], x[4,i,s] == 0) #No water in DK & DE
-
     @constraint(m,[s in S], 0 <= volume[s] <= 33*10^6) #Reservoir limits
-    
+    @constraint(m, Max_Hydro[s in S], x[4,2,s]<= volume[s])
     @constraint(m,[s in S], sum(x[i,1,s] for i in I) >= Load_DE[s]) #Load balance for Germany
     @constraint(m,[s in S], sum(x[i,2,s] for  i in I) >= Load_SE[s]) #Load balance for Sweden
     @constraint(m,[s in S], sum(x[i,3,s] for i in I) >= Load_DK[s]) #Load balance for Denmark
-
+    
     
     @constraint(m,(0.202/0.4)*sum(x[3,j,s] for s in S for j in J)<=0.1*1.29*10^8) #CO2
 
